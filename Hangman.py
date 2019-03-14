846d9f911486aa372d42a0ced6c9addffcef9348
shinben0327 <shinben0327@gmail.com>
2019-03-14 23:27:20 +0900

    Initial commit

---
+++ Hangman.py
@@ -0,0 +1,19 @@
+"""
+Created on Mar 14 2019
+@author: jihwanshin
+
+This program will let users play Hangman, the classic guessing game, on their computer!
+
+THINGS TO DO:
+showMAN()
+addMAN()
+showWORD()
+addLETTER()
+showMISS()
+addMISS()
+inWORD()
+input letter
+make list of words in words.txt
+"""
+
+print("Welcome to Hangman game!")
---
+++ README.md
@@ -0,0 +1,2 @@
+# Hangman
+This program will let users play Hangman, the classic guessing game, on their computer!
