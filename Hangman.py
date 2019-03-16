3c7486ab5a3bd4c9acbf643cc793e940fe134f44
shinben0327 <shinben0327@gmail.com>
2019-03-16 13:55:50 +0900

    Added Hangman textart and ability to choose the random word. Few words have been added to words.txt

--- Hangman.py
+++ Hangman.py
@@ -16,4 +16,104 @@ input letter
 make list of words in words.txt
 """
 
-print("Welcome to Hangman game!")
+import random
+
+print("Welcome to Hangman game!")  # initial welcome message
+
+TheMan = [  # this will contain all possible states of hangman
+    """
+    ____
+  |    |
+  |
+  |
+  |
+  |
+ _|_
+|   |______
+|          |
+|__________|
+""",
+    """
+    ____
+  |    |
+  |    o
+  |
+  |
+  |
+ _|_
+|   |______
+|          |
+|__________|
+""",
+    """
+    ____
+  |    |
+  |    o
+  |    |
+  |    |
+  |
+ _|_
+|   |______
+|          |
+|__________|
+""",
+    """
+    ____
+  |    |
+  |    o
+  |   /|
+  |    |
+  |
+ _|_
+|   |______
+|          |
+|__________|
+""",
+    """
+    ____
+  |    |
+  |    o
+  |   /|\\
+  |    |
+  |
+ _|_
+|   |______
+|          |
+|__________|
+""",
+    """
+    ____
+  |    |
+  |    o
+  |   /|\\
+  |    |
+  |   /
+ _|_
+|   |______
+|          |
+|__________|
+""",
+    """
+    ____
+  |    |
+  |    o
+  |   /|\\
+  |    |
+  |   / \\
+ _|_
+|   |______
+|          |
+|__________|
+"""
+          ]
+
+with open("words.txt") as words:  # brings the words in words.txt to this python module
+    words_string = words.read()
+    words_list = words_string.split(", ")
+
+rand_word_index = random.randint(0, len(words_list)-1)  # randomly chooses a word to be guessed
+TheWord = words_list[rand_word_index]
+
+TheLetters = []  # makes TheWord into a list of its characters
+for letter in TheWord:
+    TheLetters.append(letter)
