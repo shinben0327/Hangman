c05a5ce93ef541c1a5ab331abe2ff1f57b74a5a6
shinben0327 <shinben0327@gmail.com>
2019-04-25 23:59:10 +0900

    Added showman() function and made the base for guess() function

--- Hangman.py
+++ Hangman.py
@@ -106,6 +106,7 @@ TheMan = [  # this will contain all possible states of hangman
 |__________|
 """
           ]
+fails = 0
 
 with open("words.txt") as words:  # brings the words in words.txt to this python module
     words_string = words.read()
@@ -117,3 +118,31 @@ TheWord = words_list[rand_word_index]
 TheLetters = []  # makes TheWord into a list of its characters
 for letter in TheWord:
     TheLetters.append(letter)
+
+TheEmpty = []
+for i in range(len(TheLetters)):
+    TheEmpty.append("_")
+
+TheWrong = []
+
+def showman():
+    print(TheMan[fails])
+    print("Here is the word:")
+    print(*TheEmpty)
+    print()
+    print("Here are your incorrect guesses:")
+    print(*TheWrong)
+
+
+def guess(letter=input("Guess a letter! ")):
+    if letter in TheLetters:
+        None  # it should say it's a correct guess and add them to TheEmpty in the correct spot
+    elif letter in TheWrong:
+        None  # it should tell the user that he/she is being a little stupid, and let them guess again
+    else:
+        None  # it should tell the user that it is an incorrect guess and add one to fails
+
+
+while "_" in TheEmpty:
+    showman()
+    guess(letter=input("Guess a letter! "))
